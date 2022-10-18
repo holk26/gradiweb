1b54105d4753ef2191f615efb454c8279b316be7
HOMERO CABRERA ARAQUE <homero.cabrera@telefonica.com>
2022-10-18 15:17:29 -0500

    lo

--- src/views/404/index.js
+++ src/views/404/index.js
@@ -1,7 +1,7 @@
 export default function FourOFour() {
     return(
         <div>
-            <p>Perdona no encontre lo que estabas buscando!!!!!!!! <3 </p>
+            <p>Perdona no encontre lo que estabas buscando!!!!!!!!</p>
         </div>
     );
 }
